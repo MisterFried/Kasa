4dd87c75af831774d73177fd7125380b4f488b3f
MisterFried <wilbaspeyras@gmail.com>
2023-11-02 10:24:41 +0200

    refactor: merge app.tsx and index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import ErrorPage from "./pages/error-page";
+import Hosting from "./pages/hosting.tsx";
+
+import "./styles/styles.scss";
+import About from "./pages/about";
+import Header from "./components/header.tsx";
+import Footer from "./components/footer.tsx";
 
 // Render
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/hosting/:id" element={<Hosting />} />
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+			<Footer />
 		</BrowserRouter>
 	</React.StrictMode>
 );
